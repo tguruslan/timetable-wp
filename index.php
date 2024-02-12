262d75b94edcb5e11d4a7e4d4c435f25c625d88d
Ruslan Lopushan <tguruslan1994@gmail.com>
2024-02-12 14:14:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ add_shortcode("tmtbl_stud", function($atts, $content = null){
     extract(shortcode_atts(array("faculty_id" => '',"okr" => '',"education_form_id" => '',"lang"=> ''), $atts));  
     wp_enqueue_script('slimselect', '//unpkg.com/slim-select@latest/dist/slimselect.min.js');
     wp_enqueue_style('slimselect', '//unpkg.com/slim-select@latest/dist/slimselect.css');
-    wp_enqueue_style('timetable', plugins_url('style.css',__FILE__ ));
-    wp_enqueue_script('timetable', plugins_url('script_stud.js',__FILE__ ), array('slimselect'), null );
+    wp_enqueue_style('timetable', '//rozklad.udpu.edu.ua/css/stud.css');
+    wp_enqueue_script('timetable', '//rozklad.udpu.edu.ua/js/stud.js', array('slimselect'), null );
     $hide=[];
     if($faculty_id){array_push($hide, "faculty_id");}
     if($education_form_id){array_push($hide, "education_form_id");}
@@ -21,6 +21,3 @@ add_shortcode("tmtbl_stud", function($atts, $content = null){
     $html='<div id="timetable" hide="'.implode(';',$hide).'" faculty_id="'.$faculty_id.'" education_form_id="'.$education_form_id.'" okr="'.$okr.'" lang="'.$lang.'"></div>';
     return $html;
 });
-
-
-
