ada4cc9fea2dc70f669f30483b2224c36f0a1c47
Ruslan Lopushan <tguruslan1994@gmail.com>
2024-02-12 12:06:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: timetable
 Plugin URI: https://rozklad.udpu.edu.ua/
-Description: [tmtbl_stud faculty_id='' okr='' education_form_id='' lang='']   [tmtbl_teacher]
+Description: [tmtbl_stud faculty_id='' okr='' education_form_id='' lang='']
 Version: 1.1
 Author: Ruslan IOC
 Author URI: https://github.com/tguruslan
@@ -22,16 +22,5 @@ add_shortcode("tmtbl_stud", function($atts, $content = null){
     return $html;
 });
 
-
-
-add_shortcode("tmtbl_teacher", function($atts, $content = null){
-    extract(shortcode_atts(array("lang"=> ''), $atts));
-    wp_enqueue_script('slimselect', '//unpkg.com/slim-select@latest/dist/slimselect.min.js');
-    wp_enqueue_style('slimselect', '//unpkg.com/slim-select@latest/dist/slimselect.css');
-    wp_enqueue_style('timetable', plugins_url('style.css',__FILE__ ));
-    wp_enqueue_script('timetable', plugins_url('script_teacher.js',__FILE__ ), array('slimselect'), null );
-    $html='<div id="timetable" lang="'.$lang.'"></div>';
-    return $html;
-});
 
 
